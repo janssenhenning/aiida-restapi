4e00f1ecddd91a5a13f0f74cdf958793f52747ba
janssenhenning <henning.janssen@gmx.net>
2022-03-21 10:36:11 +0100

    Add configuration to allow ssh-tunnel setup for iffaiida

--- aiida_restapi/main.py
+++ aiida_restapi/main.py
@@ -5,7 +5,22 @@ from fastapi import FastAPI
 from aiida_restapi.graphql import main
 from aiida_restapi.routers import auth, computers, groups, nodes, process, users
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth.router)
 app.include_router(computers.router)
 app.include_router(nodes.router)
